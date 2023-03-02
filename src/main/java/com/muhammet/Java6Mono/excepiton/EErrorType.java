d44182f515064e36b6ea742bbb6123bf8ce0ed2e
Muhammet Ali KAYA <info@beemeapp.com>
2023-03-02 12:26:23 +0300

    02.03.2023 gün sonu

--- src/main/java/com/muhammet/Java6Mono/excepiton/EErrorType.java
+++ src/main/java/com/muhammet/Java6Mono/excepiton/EErrorType.java
@@ -14,6 +14,8 @@ public enum EErrorType {
 
     MUSTERI_BULUNAMADI(1003,"Aradınız müşteri sistemde kayıt değildir.", INTERNAL_SERVER_ERROR),
     URUN_EKLEME(2001,"Ürün ekleme başarısız oldu", INTERNAL_SERVER_ERROR),
+    METHOD_MIS_MATCH_ERROR(2002,"Giriş yaptığınız değer, istenilen değerle uyuşmamaktadır", BAD_REQUEST),
+    METHOD_NOT_VALID_ARGUMENT_ERROR(2003,"URL içinde eksik parametre gönderimi",BAD_REQUEST),
     INVALID_PARAMETER(3001,"Geçersiz parametre giriş yaptınız", BAD_REQUEST);
 
     private int code;
